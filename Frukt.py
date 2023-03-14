254473680ac5e9d24cfb06a5892d1f8eb9623840
Max Försth <mafo1003@storsjogymnasiet.se>
2023-03-14 10:40:46 +0100

    commit3

--- Frukt.py
+++ Frukt.py
@@ -3,12 +3,35 @@ Frukter = ("Banan","Melon","kiwi","Citron")
 Looping = True
 
 def print_fruit(userinput):
-    fnr = int(userinput):
-    print("\n" + Frukter[fnr-1] + "kommer här! \n")
+    fnr = int(userinput)
+    print("\n" + Frukter[fnr-1] + "\n kommer här! \n")
+
+
 
 while Looping:
+
+    print("------------------------")
+    print("\n-: FruktAutomat:-\n")
+
+    i=1
+
+    for Frukt in Frukter:
+        print(str(i) + ": " + Frukt)
+        i += 1
+
+    FruktNr = input("\\n Mata in nr på frukten du vill ha: ")
+
+    print_fruit(FruktNr)
+
     Go = input("Vill du välja en frukt till? j/n")
     print("\n")
 
     if(Go == "n"):
         break
+
+
+
+print("------")
+print("Du får en frukt till")
+Slumpfrukt = random.randint(1, 4)
+print_fruit(Slumpfrukt)
